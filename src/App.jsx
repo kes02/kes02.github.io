a8b2079c53f6382d0304e7b5bd094b575ee8afc8
Heejin Jung <hijin1220@gmail.com>
2025-07-29 16:53:22 +0900

    feat: App page-content section tag
    App.css의 .page-content의 section 태그 디자인 수정 및 경로 작성 오류 시 home으로 돌아오도록 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
-import Posts from "./pages/Posts";
 
 import './App.css';
 
@@ -19,7 +18,7 @@ function App() {
                             <Route path="/" element={<Home />} />
                             <Route path="/resume" element={<Resume />} />
                             <Route path="/portfolio" element={<Portfolio />} />
-                            <Route path="/posts" element={<Posts />} />
+                            <Route path="*" element={<Navigate to="/" replace />} />
                         </Routes>
                     </div>
                 </div>
